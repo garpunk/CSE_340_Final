687247ad508a91486f83009755e9d7a1410072be
garpunk <garpunked@gmail.com>
2026-03-05 15:18:32 -0700

    first commit, setting up basics

---
+++ server.js
@@ -0,0 +1,40 @@
+// Imports
+import express from 'express';
+import { fileURLToPath } from 'url';
+import path from 'path';
+
+// Variables
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// Define the port number the server will listen on
+const PORT = process.env.PORT || 3000;
+
+//Express Server
+const app = express();
+
+/**
+ * Configure Express middleware
+ */
+
+// Serve static files from the public directory
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+
+// Routes
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'src/views/home.html'));
+});
+app.get('/about', (req, res) => {
+    res.sendFile(path.join(__dirname, 'src/views/about.html'));
+});
+app.get('/products', (req, res) => {
+    res.sendFile(path.join(__dirname, 'src/views/products.html'));
+});
+
+
+// Start the server and listen on the specified port
+app.listen(PORT, () => {
+    console.log(`Server is running on http://127.0.0.1:${PORT}`);
+});
